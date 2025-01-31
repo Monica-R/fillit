2c4a624da01b417a268af910bf1445ecb7b8a836
Monica-R <moniroka_339@hotmail.com>
2025-01-31 10:01:15 +0000

    shifting the whole paradigm to POO

--- src/game.js
+++ src/game.js
@@ -1,3 +1,21 @@
+import { Game } from "./logic-code.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttonPlay = document.querySelector('.button-play');
+    let gameInstance;
+
+    buttonPlay.onclick = () => {
+        gameInstance = new Game();
+        gameInstance.start();
+    };
+});
+
+
+
+
+
+
+
 // import { game, printAttempt, wordGame, findTheChar, removeAccents } from "./prueba.js";
 
 // document.addEventListener("DOMContentLoaded", () => {
@@ -58,93 +76,5 @@
 //     }
 // });
 
-// main.js
 
-import { game, printAttempt, getWordGame, findTheChar, removeAccents } from "./logic-code.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const buttonPlay = document.querySelector('.button-play');
-    let myAttempts;
-
-    buttonPlay.onclick = () => {
-        startNewGame();
-    };
-
-    function startNewGame() {
-        myAttempts = 3; // Reiniciamos los intentos
-        game();
-        printAttempt(myAttempts);
-        initializeGame();
-
-        const controlsButtons = document.querySelectorAll('.button-char');
-        controlsButtons.forEach(button => {
-            button.addEventListener('click', handleButtonClick);
-        });
-    }
-
-    function initializeGame() {
-        const charsList = document.querySelectorAll('.char-item');
-        hideWord(charsList);
-    }
-
-    function hideWord(chars) {
-        chars.forEach(element => {
-            element.style.visibility = 'hidden';
-        });
-    }
-
-    function handleButtonClick(event) {
-        const charsList = document.querySelectorAll('.char-item');
-        const attemptsList = document.querySelectorAll('.attempt');
-        const cleanButtonValue = removeAccents(event.target.dataset.value).toUpperCase();
-        const wordGame = getWordGame();
-        const cleanWordGame = removeAccents(wordGame).toUpperCase();
-
-        if (cleanWordGame.includes(cleanButtonValue)) {
-            let foundedIndexesChar = findTheChar(wordGame, event.target.dataset["value"]);
-            showChars(charsList, foundedIndexesChar);
-        } else {
-            myAttempts--;
-            attemptsList.forEach(element => element.remove());
-            const itemAttempts = document.querySelector('.item-attempts');
-            if (itemAttempts) {
-                itemAttempts.remove();
-            }
-            printAttempt(myAttempts);
-
-            const updatedAttempts = document.querySelectorAll('.attempt');
-            if (updatedAttempts.length === 0) {
-                alert("GAME OVER");
-                resetGame();
-            }
-        }
-        event.target.setAttribute('disabled', true);
-    }
-
-    function showChars(chars, arrayIndexes) {
-        arrayIndexes.forEach(index => {
-            chars[index].style.visibility = 'visible';
-        });
-    }
-
-    function resetGame() {
-        // Removemos los event listeners de los controles
-        const controlsButtons = document.querySelectorAll('.button-char');
-        controlsButtons.forEach(button => {
-            button.removeEventListener('click', handleButtonClick);
-        });
-
-        // Limpiamos los elementos del juego
-        const containerCompletion = document.querySelector('.completion');
-        containerCompletion.innerHTML = '';
-
-        // Ocultamos la pantalla de juego y mostramos el menú principal
-        document.querySelector('.game-container').style.display = 'none';
-        document.querySelector('.principal-container').style.display = 'flex';
-
-        // Opcional: reiniciar los intentos
-        myAttempts = 3;
-    }
-});
 
