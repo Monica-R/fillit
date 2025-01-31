7524de023be95e1c40c5583c58dae6943868091b
Monica-R <moniroka_339@hotmail.com>
2025-01-31 01:05:11 +0000

    words with accents added

--- src/words.js
+++ src/words.js
@@ -44,4 +44,4 @@ export const words = [
     'templario'
 ];
 
-export const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+export const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'á', 'é', 'í', 'ó', 'ú'];
