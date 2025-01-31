7a0b0124001af3700f81796e25b2fbe62a5df863
Monica-R <moniroka_339@hotmail.com>
2025-01-31 01:08:41 +0000

    feat: startGame, hideWord, showCars functions added.

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-import { game } from "./prueba.js";
+import { game, wordGame, findTheChar, removeAccents } from "./prueba.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -6,5 +6,36 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonPlay.onclick = () => {
         console.log('holaaaa!')
         game();
+        console.log(wordGame);
+        startGame();
+        const controlsButtons = document.querySelectorAll('.button-char');
+        const charsList = document.querySelectorAll('.char-item');
+        controlsButtons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const cleanButtonValue = removeAccents(event.target.dataset.value).toUpperCase();
+                const cleanWordGame = removeAccents(wordGame).toUpperCase();
+                if (cleanWordGame.includes(cleanButtonValue)) { 
+                    let foundedIndexesChar = findTheChar(wordGame, event.target.dataset["value"]); //This is a indexes array
+                    showChars(charsList, foundedIndexesChar);
+                };
+            });
+        });
+    }
+
+    function startGame() {
+        const charsList = document.querySelectorAll('.char-item');
+        hideWord(charsList);
+    }
+
+    function hideWord(chars) {
+        return chars.forEach(element => {
+            element.style.visibility = 'hidden';
+        });
+    }
+
+    function showChars(chars, arrayIndexes) {
+        return arrayIndexes.forEach(element => {
+            chars[element].style.visibility = 'visible';
+        });
     }
 });
